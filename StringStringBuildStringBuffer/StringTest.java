14e1b5fba14e67b36c3a8e7a78edf8dd11f8055e
“MonkeyC97” <monkeyc97@163.com>
2019-05-03 20:54:29 +0800

    String StringBuffer StringBuilder

---
+++ StringStringBuildStringBuffer/StringTest.java
@@ -0,0 +1,9 @@
+package git.com.StringStringBuildStringBuffer;
+
+public class StringTest {
+    public static void main(String[] args) {
+        String str = "hehe";
+        System.out.println(str.charAt(0));
+    }
+
+}
