fbfe8c04737f566bcfdd2a9eb9d2680693680ffd
MonkeyC97 <monkeyc97@163.com>
2019-08-27 23:27:04 +0800

    【JVM】堆溢出

---
+++ JVM/Test2.java
@@ -0,0 +1,13 @@
+package Review.Test.JVM;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test2 {
+    public static void main(String[] args) {
+        List<Object> list = new ArrayList<>();
+        while(true){
+            list.add(new Object());
+        }
+    }
+}
